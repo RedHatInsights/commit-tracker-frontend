e355582fc780c8c2e5562062bb4006624c81f5a5
Ryan Long <rlong@redhat.com>
2020-08-07 13:41:00 -0400

    add releaser

---
+++ .travis/custom_release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -x
+
+if [ "${TRAVIS_BRANCH}" = "master" ]
+then
+    for env in ci qa
+    do
+        echo "PUSHING ${env}-beta"
+        rm -rf ./dist/.git
+        .travis/release.sh "${env}-beta"
+
+        echo "PUSHING ${env}-stable"
+        .travis/release.sh "${env}-stable"
+    done
+fi
