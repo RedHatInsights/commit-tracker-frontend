bad8393551df1a5f6fe83c73489c0cc68d697144
Ryan Long <rlong@redhat.com>
2021-11-01 15:03:39 -0400

    fix fed mods

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -4,7 +4,14 @@ const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../')
 });
 
-module.exports = {
-    ...webpackConfig,
-    plugins
+plugins.push(
+    require('@redhat-cloud-services/frontend-components-config/federated-modules')({
+        root: resolve(__dirname, '../'),
+    })
+  );
+  
+module.exports = (env) => {
+    env && env.analyze === 'true' && plugins.push(new BundleAnalyzerPlugin());
+
+    return { ...webpackConfig, plugins };
 };
